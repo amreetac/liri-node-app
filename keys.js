55093a39d358034dd3302099af218a912c78a2e8
Amreeta Choudhury <amreeta89@yahoo.com>
2016-07-02 17:21:50 -0400

    Created commands via switch case method and working on Twitter command

--- keys.js
+++ keys.js
@@ -1,8 +1,12 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'jO70R9AerwbegCXI0r04Iee0s',
+  consumer_secret: 'ZwE7ia0FpxAso7ACtSgeVqbzABt1TDvHNgemOKPNZyVZXTRnvm',
+  access_token_key: '54760297-FZX19wwbyQdusaNouBaS3BtwtYA4u6OObuftrWMe3',
+  access_token_secret: '2AzVIdjtfDh2pRwqmvhKNN0E6uozwWU31bv8PrVinHSKT',
 }
+
+//'<>' for the keys if this doesn't work
+
+//https://apps.twitter.com/app/12560065    http://www.amreetactwitter.com
---
+++ liri.js
@@ -0,0 +1,92 @@
+//(function () {
+
+	function liriBot () {
+
+	var fs = require('fs');
+
+	if(process.argv.length != 3) {
+			usage();
+	}
+
+	switch(process.argv[2]) {
+		case "my-tweets":
+			myTweets();
+			break;
+		case "spotify-this-song":
+			spotifyThis();
+			break;
+		case "movie-this":
+			movieThis();
+			break;
+		case "do-what-it-says":
+		doThis();
+			break;
+		default:
+				usage();
+	}
+
+
+// This block of code will read from the "movies.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data" 
+//fs.readFile("keys.js", "utf8", function(error, data) {
+
+    
+    // Then split it by commas (to make it more readable)
+    //var dataArr = data.split(',');
+
+    //for(var i = 0; i <dataArr.length; i++)
+    //{
+    	//console.log(dataArr[i]);
+    //}
+    
+//});
+
+function myTweets() {
+
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+
+var twitterKeys = keys.twitterKeys;
+ 
+var client = new Twitter({
+  consumer_key: twitterKeys.consumer_key,
+  consumer_secret: twitterKeys.consumer_secret,
+  access_token_key: twitterKeys.access_token_key,
+  access_token_secret: twitterKeys.access_token_secret
+});
+ 
+var params = {screen_name: 'amreetac'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+  	for(var i=0; i<tweets.length; i++) {
+  		//console.log(tweets);
+  		//console.log("------------");
+    	console.log("Created At:" + tweets[i].);
+    	//console.log("------------");
+  }
+}
+});
+}
+
+function spotifyThis() {
+	
+}
+
+function movieThis() {
+	
+}
+
+function doThis() {
+	
+}
+
+function usage () {
+		console.log("Usage: node liri.js <command>");
+			console.log("Where: command my-tweets, spotify-this-song, movie-this, do-what-it-says");
+	}
+
+}
+
+liriBot();
+//});
